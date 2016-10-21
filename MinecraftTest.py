3dbd5e8392d2074ce13490fdf81eb3c8d52452d4
Angelika Ophagen <ophagen@st.ovgu.de>
2016-10-21 23:31:10 +0200

    test: load mission from external xml

---
+++ MinecraftTest.py
@@ -0,0 +1,90 @@
+# ------------------------------------------------------------------------------------------------
+# Copyright (c) 2016 A. Ophagen
+# 
+# Test world for experimentation
+
+import MalmoPython
+import os
+import sys
+import time
+
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
+
+def Menger(xorg, yorg, zorg, size, blocktype, holetype):
+    #draw solid chunk
+    genstring = GenCuboid(xorg,yorg,zorg,xorg+size-1,yorg+size-1,zorg+size-1,blocktype) + "\n"
+    #now remove holes
+    unit = size
+    while (unit >= 3):
+        w=unit/3
+        for i in xrange(0, size, unit):
+            for j in xrange(0, size, unit):
+                x=xorg+i
+                y=yorg+j
+                genstring += GenCuboid(x+w,y+w,zorg,(x+2*w)-1,(y+2*w)-1,zorg+size-1,holetype) + "\n"
+                y=yorg+i
+                z=zorg+j
+                genstring += GenCuboid(xorg,y+w,z+w,xorg+size-1, (y+2*w)-1,(z+2*w)-1,holetype) + "\n"
+                genstring += GenCuboid(x+w,yorg,z+w,(x+2*w)-1,yorg+size-1,(z+2*w)-1,holetype) + "\n"
+        unit/=3
+    return genstring
+
+def GenCuboid(x1, y1, z1, x2, y2, z2, blocktype):
+    return '<DrawCuboid x1="' + str(x1) + '" y1="' + str(y1) + '" z1="' + str(z1) + '" x2="' + str(x2) + '" y2="' + str(y2) + '" z2="' + str(z2) + '" type="' + blocktype + '"/>'
+    
+with open('MinecraftWorld.xml', 'r') as myXML:
+    missionXML=myXML.read()
+
+# Create default Malmo objects:
+
+agent_host = MalmoPython.AgentHost()
+try:
+    agent_host.parse( sys.argv )
+except RuntimeError as e:
+    print 'ERROR:',e
+    print agent_host.getUsage()
+    exit(1)
+if agent_host.receivedArgument("help"):
+    print agent_host.getUsage()
+    exit(0)
+
+my_mission = MalmoPython.MissionSpec(missionXML, True)
+my_mission_record = MalmoPython.MissionRecordSpec()
+
+# Attempt to start a mission:
+max_retries = 3
+for retry in range(max_retries):
+    try:
+        agent_host.startMission( my_mission, my_mission_record )
+        break
+    except RuntimeError as e:
+        if retry == max_retries - 1:
+            print "Error starting mission:",e
+            exit(1)
+        else:
+            time.sleep(2)
+
+# Loop until mission starts:
+print "Waiting for the mission to start ",
+world_state = agent_host.getWorldState()
+while not world_state.has_mission_begun:
+    sys.stdout.write(".")
+    time.sleep(0.1)
+    world_state = agent_host.getWorldState()
+    for error in world_state.errors:
+        print "Error:",error.text
+
+print
+print "Mission running ",
+
+# Loop until mission ends:
+while world_state.is_mission_running:
+    sys.stdout.write(".")
+    time.sleep(0.1)
+    world_state = agent_host.getWorldState()
+    for error in world_state.errors:
+        print "Error:",error.text
+
+print
+print "Mission ended"
+# Mission has ended.
